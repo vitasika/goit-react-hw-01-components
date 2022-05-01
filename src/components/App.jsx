7743959028bdb38a271683c258fcad705b1f6f1b
vitasika <vitasika1@gmail.com>
2022-05-01 22:43:34 +0300

    делаю настройки профиля

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,66 @@
 // import PropTypes from 'prop-types';
+// Импорт компонентов
+import Profile from '../components/Profile';
+// Импорт условий в задании
 import user from './Profile/user.json';
-import data from './Statistics/data.json';
-import friends from './FriendList/friends.json';
-import transactions from './TransactionHistory/transactions.json';
+// import data from './Statistics/data.json';
+// import friends from './FriendList/friends.json';
+// import transactions from './TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
-    <div class="profile">
-      {/* <Profile
+    <div>
+      <Profile
         key={user.key}
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      /> */}
+      />
 
-      <div class="description">
-        <img
-          src={user.avatar}
-          // src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          class="avatar"
-        />
-        <p class="name">{user.username}</p>
-        <p class="tag">@{user.tag}</p>
-        <p class="location">{user.location}</p>
-      </div>
+      {/* <Statistics
+        title="Upload stats"
+        stats={data}
+      />;
+      <Statistics stats={data}
+      />;
+      
+      <FriendList
+        friends={friends}
+      />,
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{user.stats.followers}</span>
-        </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">{user.stats.views}</span>
-        </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{user.stats.likes}</span>
-        </li>
-      </ul>
+      <TransactionHistory
+        items={transactions}
+      />; */}
     </div>
+
+    // <div class="description">
+    //     <img
+    //       src={user.avatar}
+    //       // src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+    //       alt="User avatar"
+    //       class="avatar"
+    //     />
+    //     <p class="name">{user.username}</p>
+    //     <p class="tag">@{user.tag}</p>
+    //     <p class="location">{user.location}</p>
+    //   </div>
+
+    //   <ul class="stats">
+    //     <li>
+    //       <span class="label">Followers</span>
+    //       <span class="quantity">{user.stats.followers}</span>
+    //     </li>
+    //     <li>
+    //       <span class="label">Views</span>
+    //       <span class="quantity">{user.stats.views}</span>
+    //     </li>
+    //     <li>
+    //       <span class="label">Likes</span>
+    //       <span class="quantity">{user.stats.likes}</span>
+    //     </li>
+    //   </ul>
 
     // <div
     //   style={{
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,67 @@
 import PropTypes from 'prop-types';
 
-<div class="profile">
-  <div class="description">
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-      class="avatar"
-    />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
-  </div>
+export default function Profile({ avatar, username, tag, location, stats }) {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt="User avatar" class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">@{tag}</p>
+        <p class="location">{location}</p>
+      </div>
 
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
-    </li>
-  </ul>
-</div>;
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+// export default function Profile ({avatar, username, tag, location, stats}) {
+
+//     return (
+//         <div class="profile">
+//             <div class="description">
+//                 <img
+//                 src={avatar}
+//                 alt="User avatar"
+//                 class="avatar"
+//                 />
+//                 <p class="name">{username}</p>
+//                 <p class="tag">@{tag}</p>
+//                 <p class="location">{location}</p>
+//             </div>
+
+//             <ul class="stats">
+//                 <li>
+//                     <span class="label">Followers</span>
+//                     <span class="quantity">{stats.followers}</span>
+//                 </li>
+//                 <li>
+//                     <span class="label">Views</span>
+//                     <span class="quantity">{stats.views}</span>
+//                 </li>
+//                 <li>
+//                     <span class="label">Likes</span>
+//                     <span class="quantity">{stats.likes}</span>
+//                 </li>
+//             </ul>
+//     </div>
+//     );
+
+// };
+
+// Profile.prototype = {
+
+// };
