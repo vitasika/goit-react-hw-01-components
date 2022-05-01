f5abdc2bffdd74bcf91e77a520ffdf34703ae0d6
vitasika <vitasika1@gmail.com>
2022-05-01 20:02:17 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
-import user from './Profile/user';
-import data from './Statistics/data';
-import friends from './FriendList/friends';
-import transactions from './TransactionHistory/transactions';
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 
 export const App = () => {
