4bba6c2c31b09fa189cb71aaaab60ba5b3e65be5
vitasika <vitasika1@gmail.com>
2022-05-01 16:13:46 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import PropTypes from 'prop-types';
+import user from './Profile/user'
+
+
 export const App = () => {
   return (
     <div class="profile" style={{
@@ -10,27 +14,28 @@ export const App = () => {
       }}>
       <div class="description">
         <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          src={user.avatar}
+          // src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
           alt="User avatar"
           class="avatar"
         />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+        <p class="name">{user.username}</p>
+        <p class="tag">@{user.tag}</p>
+        <p class="location">{user.location}</p>
       </div>
 
       <ul class="stats">
         <li>
           <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+          <span class="quantity">{user.stats.followers}</span>
         </li>
         <li>
           <span class="label">Views</span>
-          <span class="quantity">2000</span>
+          <span class="quantity">{user.stats.views}</span>
         </li>
         <li>
           <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+          <span class="quantity">{user.stats.likes}</span>
         </li>
       </ul>
     </div>
