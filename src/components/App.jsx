54ed53298972ff13c59ccb43bc00378ac58e16dd
vitasika <vitasika1@gmail.com>
2022-05-03 23:24:40 +0300

    111

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,45 +26,5 @@ export const App = () => {
       <FriendList friends={friends} />,
       <TransactionHistory items={transactions} />;
     </div>
-
-    // <div class="description">
-    //     <img
-    //       src={user.avatar}
-    //       // src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-    //       alt="User avatar"
-    //       class="avatar"
-    //     />
-    //     <p class="name">{user.username}</p>
-    //     <p class="tag">@{user.tag}</p>
-    //     <p class="location">{user.location}</p>
-    //   </div>
-
-    //   <ul class="stats">
-    //     <li>
-    //       <span class="label">Followers</span>
-    //       <span class="quantity">{user.stats.followers}</span>
-    //     </li>
-    //     <li>
-    //       <span class="label">Views</span>
-    //       <span class="quantity">{user.stats.views}</span>
-    //     </li>
-    //     <li>
-    //       <span class="label">Likes</span>
-    //       <span class="quantity">{user.stats.likes}</span>
-    //     </li>
-    //   </ul>
-
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
-    // >
-    //   1. React homework template
-    // </div>
   );
 };
