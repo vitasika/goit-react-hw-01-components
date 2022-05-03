081e5705661fdda854aa5054c7d106e7052fef81
vitasika <vitasika1@gmail.com>
2022-05-03 22:37:20 +0300

    добавил условия

---
+++ src/components/FriendList/Friends.jsx
@@ -0,0 +1,7 @@
+import PropTypes from 'prop-types';
+
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>;
