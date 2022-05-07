3b6052a63994741224d5da093cbd696c85c29f79
vitasika <vitasika1@gmail.com>
2022-05-07 12:52:37 +0300

    показалась статистика

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,9 +23,11 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />;
-      <Statistics stats={data} />;{/* <FriendList friends={friends} />, */}
-      {/* <TransactionHistory items={transactions} />; */}
+      <Statistics title="Upload stats" data={data} />;
+      <Statistics data={data} />;
+      {/* <FriendList friends={friends} />,
+
+      <TransactionHistory items={transactions} />; */}
     </div>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,34 +1,24 @@
 import PropTypes from 'prop-types';
+import style from '../Statistics/Statistics.module.css';
 
-export default function Statistics({ id, label, percentage }) {
+export default function Statistics({ title, data }) {
   return (
-    <section class="statistics">
-      <h2 class="title">Upload stats</h2>
+    <section className={style.statistics}>
+      {title && <h2 className="title">Upload stats</h2>}
 
-      <ul class="stat-list">
-        <li class="item" key={id}>
-          <span class="label">{label}</span>
-          <span class="percentage">{percentage}%</span>
-        </li>
-        {/* <li class="item">
-                            <span class="label">{data.label}</span>
-                            <span class="percentage">{data.percentage}%</span>
-                        </li>
-                        <li class="item">
-                            <span class="label">{data.label}</span>
-                            <span class="percentage">{data.percentage}%</span>
-                        </li>
-                        <li class="item">
-                            <span class="label">{data.label}</span>
-                            <span class="percentage">{data.percentage}%</span>
-                        </li> */}
+      <ul className={style.statlist}>
+        {data.map(item => (
+          <li className={style.item} key={item.id}>
+            <span className={style.label}>{item.label}</span>
+            <span className={style.percentage}>{item.percentage}%</span>
+          </li>
+        ))}
       </ul>
     </section>
   );
 }
 
 Statistics.propTypes = {
-  id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  percentage: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  stste: PropTypes.object.isRequired,
 };
