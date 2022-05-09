e01329bc0b288f87b5c517609159d5fce2ce1be4
vitasika <vitasika1@gmail.com>
2022-05-09 23:29:11 +0300

    Update Transactions.jsx

--- src/components/TransactionHistory/Transactions.jsx
+++ src/components/TransactionHistory/Transactions.jsx
@@ -17,11 +17,11 @@ export default function Transactions({ id, type, amount, currency }) {
           <td>{amount}</td>
           <td>{currency}</td>
         </tr>
-        {/* <tr>
-      <td>Withdrawal</td>
-      <td>85</td>
-      <td>USD</td>
-    </tr> */}
+        <tr>
+          <td>Withdrawal</td>
+          <td>85</td>
+          <td>USD</td>
+        </tr>
       </tbody>
     </table>
   );
