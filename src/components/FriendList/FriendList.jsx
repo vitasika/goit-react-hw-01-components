efc34079c41ae53c42b642384137b3af2cc2a39a
vitasika <vitasika1@gmail.com>
2022-05-13 11:54:27 +0300

    обединил в одну папку

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import FriendListItem from '../FriendListItem/FriendListItem';
+import FriendListItem from './FriendListItem/FriendListItem';
 import styles from '../FriendList/FriendList.module.css';
 
 export default function FriendList({ friends }) {
