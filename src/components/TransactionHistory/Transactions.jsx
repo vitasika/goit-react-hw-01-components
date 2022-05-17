27169328b5ffbdbae691c73b7fbe2b3fc6349e6e
vitasika <vitasika1@gmail.com>
2022-05-17 11:11:47 +0300

    сделал одним рендерингом

--- src/components/TransactionHistory/Transactions.jsx
+++ src/components/TransactionHistory/Transactions.jsx
@@ -13,20 +13,22 @@ export default function TransactionHistory({ items }) {
       </thead>
 
       <tbody>
-        {items.map((item, index) =>
-          index % 2 === 0 ? (
-            <tr key={item.id}>
-              <td>{item.type}</td>
-              <td>{item.amount}</td>
-              <td>{item.currency}</td>
-            </tr>
-          ) : (
-            <tr key={item.id}>
-              <td className={styles.odd}>{item.type}</td>
-              <td className={styles.odd}>{item.amount}</td>
-              <td className={styles.odd}>{item.currency}</td>
+        {items.map(
+          ({ id, type, amount, currency }) => (
+            // index % 2 === 0 ?
+            <tr key={id}>
+              <td className={styles.odd}>{type}</td>
+              <td>{amount}</td>
+              <td className={styles.odd}>{currency}</td>
             </tr>
           )
+          // ) : (
+          //   <tr key={item.id}>
+          //     <td className={styles.odd}>{item.type}</td>
+          //     <td className={styles.odd}>{item.amount}</td>
+          //     <td className={styles.odd}>{item.currency}</td>
+          //   </tr>
+          // )
         )}
       </tbody>
     </table>
